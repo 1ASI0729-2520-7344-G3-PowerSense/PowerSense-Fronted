24631b12fdaaed5b69b1b42cc15d39f5671ac664
Sebastian EP <149455319+sebasepe@users.noreply.github.com>
2025-12-02 04:53:42 -0500

    feat: add set-all devices

--- src/app/inventory/devices/application/set-all-devices-status.usecase.ts
+++ src/app/inventory/devices/application/set-all-devices-status.usecase.ts
@@ -4,14 +4,17 @@ import { DeviceRepository } from '../domain/repositories/device.repository';
 
 @Injectable({ providedIn: 'root' })
 export class SetAllDevicesStatusUseCase {
+
+  // Inyección del repositorio que maneja los dispositivos
   constructor(@Inject(DEVICE_REPOSITORY) private readonly repository: DeviceRepository) {}
 
   async execute(status: 'active' | 'inactive'): Promise<void> {
+    // Obtiene todos los dispositivos del repositorio
     const all = await this.repository.list();
+
+    // Recorre cada dispositivo y actualiza su estado
     for (const device of all) {
       await this.repository.setStatus(device.id, status);
     }
   }
 }
-
-
